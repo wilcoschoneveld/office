8205b19ef49b30cdbec0788f76c1d63e691327e1
Wilco Schoneveld <schoneveld.wj@gmail.com>
2021-04-22 15:09:57 +0200

    WIP: Physics work

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import "./style.css";
 import {
+    AmmoJSPlugin,
     ArcRotateCamera,
     Engine,
     HemisphericLight,
+    Mesh,
+    PhysicsImpostor,
     Scene,
     SceneLoader,
     Vector3,
     WebXRDefaultExperience,
 } from "@babylonjs/core";
 import "@babylonjs/loaders";
-// import "@babylonjs/inspector";
+import * as Ammo from "ammo.js";
+import "@babylonjs/inspector";
 
 async function createScene(engine: Engine) {
     const scene = new Scene(engine);
@@ -24,6 +28,8 @@ async function createScene(engine: Engine) {
         scene
     );
     camera.attachControl(true);
+    camera.alpha = 0.5 * Math.PI;
+    camera.beta = 1.381;
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -36,6 +42,8 @@ async function createScene(engine: Engine) {
     scene.getMeshByName("Cube.024")!.visibility = 0;
 
     const floorMesh = scene.getMeshByName("Plane.014")!;
+    const bucket = scene.getMeshByName("Cylinder.001")!;
+    bucket.position.y += 1;
 
     const xr = await WebXRDefaultExperience.CreateAsync(scene, {
         floorMeshes: [floorMesh],
@@ -48,7 +56,25 @@ async function createScene(engine: Engine) {
         xrCamera.position.z = 7.87;
     });
 
-    // scene.debugLayer.show();
+    const gravityVector = new Vector3(0, -9.81, 0);
+    const ammoModule = await new Ammo();
+    scene.enablePhysics(gravityVector, new AmmoJSPlugin(true, ammoModule));
+
+    const floorCollider = Mesh.CreateBox("floorCollider", 1);
+    floorCollider.scaling = new Vector3(5, 0.1, 5);
+    floorCollider.position = new Vector3(-0.5, 0.5, 5.72);
+
+    const bucketCollider = Mesh.CreateBox("bucketCollider", 1);
+    bucketCollider.scaling = new Vector3(0.4, 0.6, 0.4);
+    bucketCollider.position = new Vector3(-2.4, 1.85, 4.13);
+    // bucket.parent = bucketCollider;
+    // bucket.scaling = new Vector3(1, 1, 1);
+    // bucket.position = new Vector3(0, 0, 0);
+
+    floorCollider.physicsImpostor = new PhysicsImpostor(floorCollider, PhysicsImpostor.BoxImpostor, { mass: 0 });
+    bucketCollider.physicsImpostor = new PhysicsImpostor(bucketCollider, PhysicsImpostor.BoxImpostor, { mass: 1 });
+
+    scene.debugLayer.show();
 
     return scene;
 }
